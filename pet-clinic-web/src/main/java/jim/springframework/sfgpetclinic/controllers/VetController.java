b093e72a1150ece923c1e648ccceb0419c4539f6
james smith <smithjimdev@gmail.com>
2018-10-16 17:52:22 +0100

    use BaseURL in class @RequestMapping for VetController

--- pet-clinic-web/src/main/java/jim/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/jim/springframework/sfgpetclinic/controllers/VetController.java
@@ -3,12 +3,13 @@ package jim.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping(VetController.BASE_URL)
 @Controller
 public class VetController {
 
     public static final String BASE_URL = "/vets";
 
-    @RequestMapping({VetController.BASE_URL, VetController.BASE_URL +"/index", VetController.BASE_URL+"/index.html"})
+    @RequestMapping({"", "/index", "/index.html"})
     public String listVets() {
         return "vets/index";
     }
