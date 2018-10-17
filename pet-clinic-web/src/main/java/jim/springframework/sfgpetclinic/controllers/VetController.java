262bf653548c11ac21b13c0d7190648304729cc6
james smith <smithjimdev@gmail.com>
2018-10-17 14:49:48 +0100

    List all vets on vets index page. Closes #15

--- pet-clinic-web/src/main/java/jim/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/jim/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,6 +1,8 @@
 package jim.springframework.sfgpetclinic.controllers;
 
+import jim.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(VetController.BASE_URL)
@@ -9,8 +11,15 @@ public class VetController {
 
     public static final String BASE_URL = "/vets";
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 
