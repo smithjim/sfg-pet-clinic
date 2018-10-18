567a230a5499b450b21d728ddaee000f8ebace1e
james smith <smithjimdev@gmail.com>
2018-10-18 17:00:21 +0100

    added PetTypes to bootstrap dataloader. Closes #32

--- pet-clinic-web/src/main/java/jim/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/jim/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,11 @@
 package jim.springframework.sfgpetclinic.bootstrap;
 
 import jim.springframework.sfgpetclinic.model.Owner;
+import jim.springframework.sfgpetclinic.model.PetType;
 import jim.springframework.sfgpetclinic.model.Vet;
 import jim.springframework.sfgpetclinic.services.OwnerService;
+import jim.springframework.sfgpetclinic.services.PetService;
+import jim.springframework.sfgpetclinic.services.PetTypeService;
 import jim.springframework.sfgpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,14 +18,22 @@ public class DataLoader implements CommandLineRunner {
 
     private final VetService vetService;
 
+    private final  PetTypeService petTypeService;
+
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = createPetType("dog");
+        PetType cat = createPetType("cat");
+        petTypeService.save(dog);
+        petTypeService.save(cat);
 
         Owner owner1 = createOwner("owner", "one");
         Owner owner2 = createOwner( "owner", "two");
@@ -35,6 +46,12 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet2);
 
 
+    }
+
+    private PetType createPetType(String name) {
+        PetType petType = new PetType();
+        petType.setName(name);
+        return petType;
     }
 
     private Owner createOwner(String fname, String lname) {
