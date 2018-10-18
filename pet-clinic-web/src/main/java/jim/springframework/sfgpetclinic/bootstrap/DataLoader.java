2a8ca51628e9d4d91510daefa38bb6fc37f569e1
james smith <smithjimdev@gmail.com>
2018-10-18 17:20:28 +0100

    added pets and contact info to owners on startup. Broken Single responsibility OwnerServiceMap. Closes #33

--- pet-clinic-web/src/main/java/jim/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/jim/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package jim.springframework.sfgpetclinic.bootstrap;
 
 import jim.springframework.sfgpetclinic.model.Owner;
+import jim.springframework.sfgpetclinic.model.Pet;
 import jim.springframework.sfgpetclinic.model.PetType;
 import jim.springframework.sfgpetclinic.model.Vet;
 import jim.springframework.sfgpetclinic.services.OwnerService;
@@ -10,6 +11,11 @@ import jim.springframework.sfgpetclinic.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -35,10 +41,18 @@ public class DataLoader implements CommandLineRunner {
         petTypeService.save(dog);
         petTypeService.save(cat);
 
-        Owner owner1 = createOwner("owner", "one");
-        Owner owner2 = createOwner( "owner", "two");
+        Owner owner1 = createOwner("owner", "one", "address1", "city1", "111-111");
+        Pet pet1 = createPet("Russel", owner1, dog, LocalDate.of(1990, 1, 1));
+        owner1.setPets(new HashSet<>(Arrays.asList(pet1)));
+
+        Owner owner2 = createOwner( "owner", "two", "address2", "city1", "222-222");
+        Pet pet2 = createPet("Catherine", owner2, cat, LocalDate.of(2010, 6, 7));
+        owner2.setPets(new HashSet<>(Arrays.asList(pet2)));
+
         ownerService.save(owner1);
         ownerService.save(owner2);
+
+
 
         Vet vet1 = createVet( "vet", "one");
         Vet vet2 = createVet( "vet", "two");
@@ -54,10 +68,22 @@ public class DataLoader implements CommandLineRunner {
         return petType;
     }
 
-    private Owner createOwner(String fname, String lname) {
+    private Pet createPet(String name, Owner o, PetType pType, LocalDate date) {
+        Pet p = new Pet();
+        p.setName(name);
+        p.setPetType(pType);
+        p.setOwner(o);
+        p.setBirthDate(date);
+        return p;
+    }
+
+    private Owner createOwner(String fname, String lname, String address, String city, String telephone) {
         Owner o = new Owner();
         o.setFirstName(fname);
         o.setLastName(lname);
+        o.setAddress(address);
+        o.setCity(city);
+        o.setTelephone(telephone);
         return o;
     }
 
