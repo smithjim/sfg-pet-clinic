2a161a29e2ea4f725e72dc68d6a7d6c4f9a1fbc8
james smith <smithjimdev@gmail.com>
2018-10-18 18:20:00 +0100

    fixed broken links. Closes #45

--- pet-clinic-web/src/main/java/jim/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/jim/springframework/sfgpetclinic/controllers/VetController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping(VetController.BASE_URL)
+@RequestMapping({VetController.BASE_URL, VetController.BASE_URL + ".html"})
 @Controller
 public class VetController {
 
