0c7e068db4b4c4ddd9193cf2c4d816237ea38b6b
james smith <smithjimdev@gmail.com>
2018-10-23 15:11:10 +0100

    updated owner to JPA entities. Closes #36

--- pet-clinic-data/src/main/java/jim/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/jim/springframework/sfgpetclinic/model/Owner.java
@@ -1,13 +1,20 @@
 package jim.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
+@Table(name="owners")
 public class Owner extends Person {
 
+    @Column(name="address")
     private String address;
+    @Column(name="city")
     private String city;
+    @Column(name="telephone")
     private String telephone;
 
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets;
 
     public String getAddress() {
