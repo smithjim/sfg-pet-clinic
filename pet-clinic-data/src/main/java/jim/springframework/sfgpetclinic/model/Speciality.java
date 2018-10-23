1b9877b5cb5a8174ed85667b6dae3a109936a9db
james smith <smithjimdev@gmail.com>
2018-10-23 15:16:26 +0100

    updated vet to JPA entity. Closes #37

--- pet-clinic-data/src/main/java/jim/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/jim/springframework/sfgpetclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package jim.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="speciaility")
 public class Speciality extends BaseEntity {
 
+    @Column(name="description")
     private String description;
 
     public String getDescription() {
